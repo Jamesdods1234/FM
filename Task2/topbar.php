811536e2c6483f6e55780cf7833741fc7ea56f39
Jamesdods1234 <jamesdodimead14@hotmail.com>
2026-03-22 17:02:47 +0000

    Add files via upload

---
+++ Task2/topbar.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html> 
+<head>
+
+<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+<nav class="box">
+<h1 style="font-family:African;">Welcome to Ridget Zoo Adventures
+<a href="home.php" title="This is the homepage">  Home </a>
+<a href="zoobook.php" title="This is the zoo ticket page"> Zoo Tickets </a>
+<a href="hotel.php" title="This is the Hotel Booking page"> Hotel Booking  </a>
+<a href="education.php"  title="This is the Educational page "> Educational </a>
+<a href="profile.php"  title="This is the Profile Page"> Account </a>
+<a href="login.php" title="This is the login page"> Login</a>
+<a href="about.php"  title="This is the About Page"> About</a>
+</h1> 
+</nav>
+
+</body>
+</html>
